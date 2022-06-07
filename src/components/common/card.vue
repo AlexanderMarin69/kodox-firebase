81872fa466b4a181feab24dc8506d1a4b32a6a09
Alexander Marin <alexander@netmine.se>
2022-06-07 09:37:59 +0200

    card peek iframe test

--- src/components/common/card.vue
+++ src/components/common/card.vue
@@ -1,72 +1,82 @@
 ﻿<template>
-    <div class="card" :style="cardGlobalStyle() + 'cursor:pointer;'" id="lmaoCard" @click="navigate()">
-    <!--<div class="card" :style="cardGlobalStyle()" id="lmaoCard">-->
+    <div>
+        <div :class="'card ' + to" :style="cardGlobalStyle() + 'cursor:pointer;'" :id="to" @click="navigate()">
+        <!--<div class="card" :style="cardGlobalStyle()" id="lmaoCard">-->
             <div class="card-body">
-                <div class="row">
-                    <!-- 7 -->
-                    <div class="col-10">
-                        <h6 class="card-title">
-                            <span class="material-symbols-outlined" style="background:transparent !important; vertical-align: bottom;">{{ tag }}</span>
-                            {{ title }}
-                        </h6>
-                        <div class="row">
-                            <div class="col-12">
-                                <p v-if="spaceType" class="text-secondary">{{spaceType}}</p>
+                    <div class="row">
+                        <!-- 7 -->
+                        <div class="col-10">
+                            <h6 class="card-title">
+                                <span class="material-symbols-outlined" style="background:transparent !important; vertical-align: bottom;">{{ tag }}</span>
+                                {{ title }}
+                            </h6>
+                            <div class="row">
+                                <div class="col-12">
+                                    <p v-if="spaceType" class="text-secondary">{{spaceType}}</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- 3 -->
+                        <div class="col-3" v-if="spaceType">
+                            <div class="avatars">
+                                <span class="avatar" v-for="item in members" :key="item.id">
+                                    <img src="../../assets/placeholder.jpg">
+                                </span>
+                            </div>
+                        </div>
+
+                        <!-- 2 -->
+                        <!--<div class="col-1">
+                            <p class="float-right">
+                                <span class="material-symbols-outlined" style="background:transparent !important;">{{ tag }}</span>
+                            </p>
+                        </div>-->
+                    <div class="col-2 text-right" style="padding-right: 0px !important; margin-top: -5px !important;">
+                        <div class="dropdown">
+                            <button :style="cardGlobalStyle()" class="dropdown-toggle text-right" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <span class="material-symbols-outlined" style="vertical-align: bottom;">
+                                    more_vert
+                                </span>
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <button class="dropdown-item" @click.stop="peekArticle()">
+                                    <span class="material-symbols-outlined" style="vertical-align: bottom;">
+                                        visibility
+                                    </span>
+                                    Peek
+                                </button>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
                             </div>
                         </div>
                     </div>
-
-                    <!-- 3 -->
-                    <div class="col-3" v-if="spaceType">
-                        <div class="avatars">
-                            <span class="avatar" v-for="item in members" :key="item.id">
-                                <img src="../../assets/placeholder.jpg">
-                            </span>
-                        </div>
                     </div>
-
-                    <!-- 2 -->
-                    <!--<div class="col-1">
-                        <p class="float-right">
-                            <span class="material-symbols-outlined" style="background:transparent !important;">{{ tag }}</span>
-                        </p>
-                    </div>-->
-                <div class="col-2 text-right" style="padding-right: 0px !important; margin-top: -5px !important;">
-                    <div class="dropdown">
-                        <button :style="cardGlobalStyle()" class="dropdown-toggle text-right" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="material-symbols-outlined" style="vertical-align: bottom;">
-                                more_vert
-                            </span>
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
-                </div>
-                </div>
              
-                <p class="card-text text-info card-description">{{ description }}</p>
-                <p style="margin-bottom: 0px;" class="text-secondary card-last-edited">
-                    <span class="material-symbols-outlined" style="vertical-align: bottom;">
-                        description
-                    </span>
-                    16 articles
-                    <br />
-                    <span class="material-symbols-outlined" style="vertical-align: bottom;">
-                        person
-                    </span>
-                    {{createdBy}}
-                    <br />
-                    <span class="material-symbols-outlined" style="vertical-align: bottom;">
-                        edit
-                    </span>
-                    {{edited}}
-                </p>
-                <slot></slot>
+                    <p class="card-text text-info card-description">{{ description }}</p>
+                    <p style="margin-bottom: 0px;" class="text-secondary card-last-edited">
+                        <span class="material-symbols-outlined" style="vertical-align: bottom;">
+                            description
+                        </span>
+                        16 articles
+                        <br />
+                        <span class="material-symbols-outlined" style="vertical-align: bottom;">
+                            person
+                        </span>
+                        {{createdBy}}
+                        <br />
+                        <span class="material-symbols-outlined" style="vertical-align: bottom;">
+                            edit
+                        </span>
+                        {{edited}}
+                    </p>
+                    <slot></slot>
             </div>
         </div>
+        <div id="iframe">
+            <iframe src="http://192.168.20.152:8080/article-view/7RZHRlJDVmfR10CLtZff" title="W3Schools Free Online Web Tutorials"></iframe>
+        </div>
+    </div>
 </template>
 
 
@@ -74,18 +84,25 @@
     button:focus {
         outline: none;
     }
+
     .dropdown-toggle {
         background: transparent !important;
         border: 0px;
         cursor: pointer;
         padding: 6px;
     }
+
     .dropdown-toggle:after {
         content: none
     }
+
     .dropdown-toggle:hover {
         background: rgba(255, 255, 255, 0.4) !important;
         border-radius: 50%;
+    }
+
+    #iframe {
+        display: none;
     }
 
     .card:hover {
@@ -153,8 +170,12 @@
                 hasMembers: false
             };
         },
-
         methods: {
+            peekArticle() {
+                console.log('peek');
+                document.getElementById(this.to).style.display = "none";
+                document.getElementById("iframe").style.display = "block";
+            },
             navigate() {
                 this.$router.push({ name: 'article-view', params: { id: this.to } });
             },
